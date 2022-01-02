41c2f2082380b6c1e7c311d9e9cbe465e5fadd81
Shivam jindal <73436052+yes-its-shivam@users.noreply.github.com>
2022-01-02 16:03:56 +0530

    Delete report.py

--- report.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pandas_profiling import ProfileReport
-
-data = pd.read_csv('internship companies.csv')
-data = data.drop('Unnamed: 0',axis=1)
-profile = ProfileReport(data, title='report company')
-profile.to_file('internship report.html')
